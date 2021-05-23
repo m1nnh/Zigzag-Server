e1c02319f7d5f441b1ad4d2d67d3cfcbb16e69df
m1nnh <alsgur9784@Naver.com>
2021-05-23 14:34:30 +0900

    Add : Zigzag

--- src/app/User/userController.js
+++ src/app/User/userController.js
@@ -7,14 +7,6 @@ const {response, errResponse} = require("../../../config/response");
 const regexEmail = require("regex-email");
 const {emit} = require("nodemon");
 
-/**
- * API No. 0
- * API Name : 테스트 API
- * [GET] /app/test
- */
-// exports.getTest = async function (req, res) {
-//     return res.send(response(baseResponse.SUCCESS))
-// }
 
 /**
  * API No. 1
