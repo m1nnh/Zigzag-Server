14bb191ce12af4050b47d607eca00cb33b3b2778
minhyeok <alsgur9784@naver.com>
2021-06-04 11:45:13 +0900

    Add : Zigzag

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ if (process.env.NODE_ENV === 'development') {
 
 express().listen(port);
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
+
+schedule.scheduleJob('0 0 18 * * *', async function() {
+    console.log('현재 ' + new Date());
+   
+
+});
+
+schedule.scheduleJob('0 0 0 * * *', async function() {
+    let now = new Date();
+    let today = now.getFullYear()+ '-' + (now.getMonth()+1).toString().padStart(2,'0') + '-' + now.getDate().toString().padStart(2,'0');
+    let yesterday = new Date(now.setDate(now.getDate() - 1));
+    yesterday = yesterday.getFullYear()+ '-' + (yesterday.getMonth()+1).toString().padStart(2,'0') + '-' + yesterday.getDate().toString().padStart(2,'0');
+
+    }
+);
+
